74c4b42a9129a283a2267ac66528be3e5d92516f
Piotr Janosz <janosz128@gmail.com>
2019-09-05 02:00:52 +0200

    Fixed value in prettierrc. Formatted files edited in previous commits according to the new prettier config.

--- ts/components/docs/search/autocomplete.tsx
+++ ts/components/docs/search/autocomplete.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import Autosuggest from 'react-autosuggest';
-import {
-  connectAutoComplete,
-  Highlight,
-  Snippet
-} from 'react-instantsearch-dom';
+import { connectAutoComplete, Highlight, Snippet } from 'react-instantsearch-dom';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { scroller } from 'react-scroll';
 
@@ -18,200 +14,177 @@ import { getNameToSearchIndex } from 'ts/utils/algolia_constants';
 import { environments } from 'ts/utils/environments';
 
 export interface IHit {
-  description: string;
-  difficulty?: string;
-  externalUrl?: string;
-  hash?: string;
-  id: number | string;
-  isCommunity?: boolean;
-  isFeatured?: boolean;
-  objectID: string;
-  sectionUrl: string;
-  tags?: string[];
-  textContent: string;
-  title: string;
-  type?: string;
-  url?: string;
-  urlWithHash?: string;
-  _highlightResult?: any;
-  _snippetResult?: any;
+    description: string;
+    difficulty?: string;
+    externalUrl?: string;
+    hash?: string;
+    id: number | string;
+    isCommunity?: boolean;
+    isFeatured?: boolean;
+    objectID: string;
+    sectionUrl: string;
+    tags?: string[];
+    textContent: string;
+    title: string;
+    type?: string;
+    url?: string;
+    urlWithHash?: string;
+    _highlightResult?: any;
+    _snippetResult?: any;
 }
 
 interface IAutoCompleteProps extends RouteComponentProps<{}> {
-  isHome?: boolean;
-  hits?: IHits[];
-  currentRefinement?: string;
-  refine?: (value: string) => void;
+    isHome?: boolean;
+    hits?: IHits[];
+    currentRefinement?: string;
+    refine?: (value: string) => void;
 }
 
 interface IHits {
-  index: string;
-  hits: IHit[];
+    index: string;
+    hits: IHit[];
 }
 
 const CustomAutoComplete: React.FC<IAutoCompleteProps> = ({
-  isHome = false,
-  hits = [],
-  currentRefinement,
-  refine,
-  history,
-  location
+    isHome = false,
+    hits = [],
+    currentRefinement,
+    refine,
+    history,
+    location,
 }) => {
-  const [value, setValue] = React.useState<string>('');
-  const [hasOverlay, setHasOverlay] = React.useState<boolean>(false);
-  let inputRef: HTMLInputElement;
+    const [value, setValue] = React.useState<string>('');
+    const [hasOverlay, setHasOverlay] = React.useState<boolean>(false);
+    let inputRef: HTMLInputElement;
 
-  React.useEffect(() => {
-    const handleKeyUp: any = (event: React.KeyboardEvent): void => {
-      if (event.key === 'Escape') {
-        setValue('');
-        inputRef.blur();
-      }
+    React.useEffect(() => {
+        const handleKeyUp: any = (event: React.KeyboardEvent): void => {
+            if (event.key === 'Escape') {
+                setValue('');
+                inputRef.blur();
+            }
+        };
+
+        window.addEventListener('keyup', handleKeyUp);
+        return () => {
+            window.removeEventListener('keyup', handleKeyUp);
+        };
+    }, []);
+
+    const onSuggestionsFetchRequested = ({ value: newValue }: { value: string }): void => refine(newValue);
+
+    const onSuggestionsClearRequested = (): void => refine('');
+
+    const onSuggestionSelected = (event: React.KeyboardEvent, { suggestion }: { suggestion: IHit }): void => {
+        const { externalUrl, hash, url, urlWithHash } = suggestion;
+
+        if (!externalUrl) {
+            // If there is a hash (fragment identifier) and the user is currently
+            // on the same page, scroll to content. If not, route away to the doc page.
+            if (hash && location.pathname === url) {
+                const id = hash.substring(1); // Get rid of # symbol
+                scroller.scrollTo(id, {
+                    smooth: true,
+                    duration: docs.scrollDuration,
+                    offset: -docs.headerOffset,
+                });
+            } else {
+                history.push(urlWithHash);
+                window.scrollTo(0, 0);
+            }
+        }
+
+        setValue(''); // Clear input value
+        inputRef.blur(); // Blur input
     };
 
-    window.addEventListener('keyup', handleKeyUp);
-    return () => {
-      window.removeEventListener('keyup', handleKeyUp);
+    const storeInputRef = (autosuggest: any): void => {
+        if (autosuggest !== null) {
+            inputRef = autosuggest.input;
+        }
     };
-  }, []);
 
-  const onSuggestionsFetchRequested = ({
-    value: newValue
-  }: {
-    value: string;
-  }): void => refine(newValue);
+    const onChange = (event: React.KeyboardEvent, { newValue, method }: { newValue: string; method: string }): void => {
+        // Only set value if the user typed it in, without it it leads to populating the input with snippet or highlight text
+        if (method === 'type') {
+            setValue(newValue);
+        }
+    };
 
-  const onSuggestionsClearRequested = (): void => refine('');
+    const onFocus = () => setHasOverlay(true);
+    const onBlur = () => setHasOverlay(false);
 
-  const onSuggestionSelected = (
-    event: React.KeyboardEvent,
-    { suggestion }: { suggestion: IHit }
-  ): void => {
-    const { externalUrl, hash, url, urlWithHash } = suggestion;
+    const inputProps = {
+        placeholder: 'Search docs…',
+        onChange,
+        onFocus,
+        onBlur,
+        value,
+    };
 
-    if (!externalUrl) {
-      // If there is a hash (fragment identifier) and the user is currently
-      // on the same page, scroll to content. If not, route away to the doc page.
-      if (hash && location.pathname === url) {
-        const id = hash.substring(1); // Get rid of # symbol
-        scroller.scrollTo(id, {
-          smooth: true,
-          duration: docs.scrollDuration,
-          offset: -docs.headerOffset
-        });
-      } else {
-        history.push(urlWithHash);
-        window.scrollTo(0, 0);
-      }
-    }
+    const hasNoSuggestions = value.length > 0 && !hits.find((hit: IHits): any => hit.hits.length);
 
-    setValue(''); // Clear input value
-    inputRef.blur(); // Blur input
-  };
-
-  const storeInputRef = (autosuggest: any): void => {
-    if (autosuggest !== null) {
-      inputRef = autosuggest.input;
-    }
-  };
-
-  const onChange = (
-    event: React.KeyboardEvent,
-    { newValue, method }: { newValue: string; method: string }
-  ): void => {
-    // Only set value if the user typed it in, without it it leads to populating the input with snippet or highlight text
-    if (method === 'type') {
-      setValue(newValue);
-    }
-  };
-
-  const onFocus = () => setHasOverlay(true);
-  const onBlur = () => setHasOverlay(false);
-
-  const inputProps = {
-    placeholder: 'Search docs…',
-    onChange,
-    onFocus,
-    onBlur,
-    value
-  };
-
-  const hasNoSuggestions =
-    value.length > 0 && !hits.find((hit: IHits): any => hit.hits.length);
-
-  return (
-    <>
-      <AutocompleteWrapper
-        hasOverlay={hasOverlay}
-        hasNoSuggestions={hasNoSuggestions}
-        isHome={isHome}>
-        <Autosuggest
-          suggestions={hits}
-          ref={storeInputRef}
-          multiSection={true}
-          focusInputOnSuggestionClick={false}
-          onSuggestionSelected={onSuggestionSelected}
-          onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
-          renderSectionTitle={renderSectionTitle}
-          getSectionSuggestions={getSectionSuggestions}
-        />
-        {hasNoSuggestions && (
-          <div className="react-autosuggest__empty">
-            No results found for "{currentRefinement}"
-          </div>
-        )}
-      </AutocompleteWrapper>
-      {hasOverlay && (
-        <AutocompleteOverlay onClick={onBlur} shouldLockScroll={!isHome} />
-      )}
-    </>
-  );
+    return (
+        <>
+            <AutocompleteWrapper hasOverlay={hasOverlay} hasNoSuggestions={hasNoSuggestions} isHome={isHome}>
+                <Autosuggest
+                    suggestions={hits}
+                    ref={storeInputRef}
+                    multiSection={true}
+                    focusInputOnSuggestionClick={false}
+                    onSuggestionSelected={onSuggestionSelected}
+                    onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={onSuggestionsClearRequested}
+                    getSuggestionValue={getSuggestionValue}
+                    renderSuggestion={renderSuggestion}
+                    inputProps={inputProps}
+                    renderSectionTitle={renderSectionTitle}
+                    getSectionSuggestions={getSectionSuggestions}
+                />
+                {hasNoSuggestions && (
+                    <div className="react-autosuggest__empty">No results found for "{currentRefinement}"</div>
+                )}
+            </AutocompleteWrapper>
+            {hasOverlay && <AutocompleteOverlay onClick={onBlur} shouldLockScroll={!isHome} />}
+        </>
+    );
 };
 
 const getSuggestionValue = (hit: IHit): string => hit.textContent;
 
 const renderSuggestion = (hit: IHit): React.ReactNode => {
-  const { externalUrl, urlWithHash } = hit;
-  const to = externalUrl ? externalUrl : urlWithHash;
-  // The atrributes to snippet are set in algolia_constants
-  const attributeToSnippet = externalUrl ? 'description' : 'textContent';
+    const { externalUrl, urlWithHash } = hit;
+    const to = externalUrl ? externalUrl : urlWithHash;
+    // The atrributes to snippet are set in algolia_constants
+    const attributeToSnippet = externalUrl ? 'description' : 'textContent';
 
-  return (
-    <Link shouldOpenInNewTab={externalUrl ? true : false} to={to}>
-      <Highlight attribute="title" hit={hit} nonHighlightedTagName="h6" />
-      <Snippet
-        attribute={attributeToSnippet}
-        hit={hit}
-        nonHighlightedTagName="p"
-        tagName="span"
-      />
-    </Link>
-  );
+    return (
+        <Link shouldOpenInNewTab={externalUrl ? true : false} to={to}>
+            <Highlight attribute="title" hit={hit} nonHighlightedTagName="h6" />
+            <Snippet attribute={attributeToSnippet} hit={hit} nonHighlightedTagName="p" tagName="span" />
+        </Link>
+    );
 };
 
 const renderSectionTitle = (section: any): React.ReactNode => {
-  // TODO(fabio): Add `api-explorer` below once the API Explore page is ready (ditto in search_input.tsx)
-  const nameToSearchIndex = getNameToSearchIndex(environments.getEnvironment());
-  const { tools, guides } = nameToSearchIndex;
-  const coreConcepts = nameToSearchIndex['core-concepts'];
+    // TODO(fabio): Add `api-explorer` below once the API Explore page is ready (ditto in search_input.tsx)
+    const nameToSearchIndex = getNameToSearchIndex(environments.getEnvironment());
+    const { tools, guides } = nameToSearchIndex;
+    const coreConcepts = nameToSearchIndex['core-concepts'];
 
-  const titles: { [key: string]: string } = {
-    // TODO: Add this back in when api - explorer page is ready
-    // to be indexed and included in the search results (ditto in search_input.tsx)
-    // [apiExplorer]: 'Api explorer',
-    [coreConcepts]: 'Core concepts',
-    [tools]: 'Tools',
-    [guides]: 'Guides'
-  };
+    const titles: { [key: string]: string } = {
+        // TODO: Add this back in when api - explorer page is ready
+        // to be indexed and included in the search results (ditto in search_input.tsx)
+        // [apiExplorer]: 'Api explorer',
+        [coreConcepts]: 'Core concepts',
+        [tools]: 'Tools',
+        [guides]: 'Guides',
+    };
 
-  if (section.hits.length) {
-    return <p>{titles[section.index]}</p>;
-  }
-  return null;
+    if (section.hits.length) {
+        return <p>{titles[section.index]}</p>;
+    }
+    return null;
 };
 
 const getSectionSuggestions = (section: any): string => section.hits;
--- ts/components/docs/sidebar/collapse.tsx
+++ ts/components/docs/sidebar/collapse.tsx
@@ -5,72 +5,68 @@ import { colors } from 'ts/style/colors';
 import { useDimensions } from 'ts/style/dimensions';
 
 interface ICollapseProps {
-  children: React.ReactNode;
-  title?: string;
+    children: React.ReactNode;
+    title?: string;
 }
 
 interface ICollapse {
-  isActive: boolean;
-  height?: number;
+    isActive: boolean;
+    height?: number;
 }
 
 export const Collapse: React.FC<ICollapseProps> = props => {
-  const [isActive, setIsActive] = React.useState<boolean>(true);
-  // @ts-ignore
-  const [contentRef, { height }] = useDimensions();
+    const [isActive, setIsActive] = React.useState<boolean>(true);
+    // @ts-ignore
+    const [contentRef, { height }] = useDimensions();
 
-  const toggleCollapse = () => {
-    setIsActive(!isActive);
-  };
+    const toggleCollapse = () => {
+        setIsActive(!isActive);
+    };
 
-  return (
-    <>
-      <CollapseToggle isActive={isActive} onClick={toggleCollapse}>
-        <p>On this page</p>
-        <svg
-          width="12"
-          height="8"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <path d="M1 7l5-5 5 5" stroke={colors.brandDark} strokeWidth="1.5" />
-        </svg>
-      </CollapseToggle>
-      <CollapseWrapper height={height} isActive={isActive}>
-        <CollapseContent isActive={isActive}>
-          <div ref={contentRef}>{props.children}</div>
-        </CollapseContent>
-      </CollapseWrapper>
-    </>
-  );
+    return (
+        <>
+            <CollapseToggle isActive={isActive} onClick={toggleCollapse}>
+                <p>On this page</p>
+                <svg width="12" height="8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 7l5-5 5 5" stroke={colors.brandDark} strokeWidth="1.5" />
+                </svg>
+            </CollapseToggle>
+            <CollapseWrapper height={height} isActive={isActive}>
+                <CollapseContent isActive={isActive}>
+                    <div ref={contentRef}>{props.children}</div>
+                </CollapseContent>
+            </CollapseWrapper>
+        </>
+    );
 };
 
 const CollapseWrapper = styled.div<ICollapse>`
-  min-height: ${props => (props.isActive ? props.height : 0)}px;
-  height: ${props => !props.isActive && '0px'};
-  display: flex;
-  flex-direction: column;
+    min-height: ${props => (props.isActive ? props.height : 0)}px;
+    height: ${props => !props.isActive && '0px'};
+    display: flex;
+    flex-direction: column;
 `;
 
 const CollapseToggle = styled.button<ICollapse>`
-  display: flex;
-  align-items: center;
-  border: none;
-  background: none;
-  cursor: pointer;
-  color: ${colors.brandDark};
-  padding: 0;
-  font-size: 0.89rem;
-  margin-bottom: ${props => (props.isActive ? 1 : 0)}rem;
+    display: flex;
+    align-items: center;
+    border: none;
+    background: none;
+    cursor: pointer;
+    color: ${colors.brandDark};
+    padding: 0;
+    font-size: 0.89rem;
+    margin-bottom: ${props => (props.isActive ? 1 : 0)}rem;
 
-  svg {
-    margin-left: 10px;
-    transform: rotate(${props => (props.isActive ? 0 : 180)}deg);
-    transition: transform 300ms ease-out;
-  }
+    svg {
+        margin-left: 10px;
+        transform: rotate(${props => (props.isActive ? 0 : 180)}deg);
+        transition: transform 300ms ease-out;
+    }
 `;
 
 const CollapseContent = styled.div<ICollapse>`
-  overflow: hidden;
-  transition: flex 300ms ease-out;
-  flex: ${props => (props.isActive ? 1 : 0)};
+    overflow: hidden;
+    transition: flex 300ms ease-out;
+    flex: ${props => (props.isActive ? 1 : 0)};
 `;
