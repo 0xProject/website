b982e54a6c1522d7923a2ae409209bf5910a4ef8
Rens <hi@rensverschuren.com>
2019-09-20 16:33:30 +0200

    Add Market Maker Profile page, add basic chart

---
+++ ts/pages/staking/market_maker_profile.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+import { HistoryChart } from 'ts/components/staking/history_chart';
+import { StakingPageLayout } from 'ts/components/staking/layout/staking_page_layout';
+
+export interface MarketMakerProfileProps {}
+
+const Container = styled.div`
+    max-width: 1152px;
+    margin: 0 auto;
+    position: relative;
+    height: 290px;
+`;
+
+export const MarketMakerProfile: React.FC<MarketMakerProfileProps> = props => {
+    return (
+        <StakingPageLayout isHome={true} title="Market Maker Profile">
+            <Container>
+                <HistoryChart />
+            </Container>
+        </StakingPageLayout>
+    );
+};
