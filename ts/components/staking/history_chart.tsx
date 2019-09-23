527486774f185a05901cfce796d1792702812aee
Rens <hi@rensverschuren.com>
2019-09-23 16:47:54 +0200

    Add second dataset, improve chart styling

--- ts/components/staking/history_chart.tsx
+++ ts/components/staking/history_chart.tsx
@@ -1,43 +1,57 @@
 import * as React from 'react';
-import { Line } from 'react-chartjs-2';
+
+import { merge } from 'lodash';
+import { defaults, Line } from 'react-chartjs-2';
 
 import { colors } from 'ts/style/colors';
 
 export interface HistoryChartProps {}
 
+const getDefaultDataset = (color: string) => {
+    return {
+        label: 'Fees collected',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: color,
+        borderColor: color,
+        borderDashOffset: 0,
+        borderWidth: 2,
+        pointBorderColor: color,
+        pointBackgroundColor: color,
+        pointBorderWidth: 0,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: color,
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 4,
+        pointHitRadius: 10,
+    }
+};
+
 const data = {
     labels: ['1 July', '5 July', '10 July', '15 July', '20 July', '25 July', '30 July'],
     datasets: [
         {
-            label: 'Fees collected',
-            fill: false,
-            lineTension: 0,
-            backgroundColor: 'rgba(0,0,0,0)',
-            borderColor: colors.brandLight,
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: colors.brandLight,
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: colors.brandLight,
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
+            ...getDefaultDataset(colors.brandLight),
             data: [41, 40, 41, 40, 41, 40, 41, 40, 41, 40, 41, 40],
+        },
+        {
+            ...getDefaultDataset('#A2F5EB'),
+            data: [31, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30],
         },
     ],
 };
 
 const options = {
-    defaultFontSize: 14,
+
     maintainAspectRatio: false,
     scales: {
         yAxes: [
             {
+                scaleLabel: {
+                    labelString: 'Fees/rewards (ETH)',
+
+                },
                 gridLines: {
                     color: 'transparent',
                 },
@@ -45,6 +59,9 @@ const options = {
                     suggestedMin: 20,
                     suggestedMax: 50,
                     stepSize: 10,
+                    callBack: (value: string, _index: number, _values: string[]) => {
+                        return 'hoi';
+                    },
                 },
             },
         ],
@@ -63,6 +80,13 @@ const options = {
         },
     },
 };
+
+merge(defaults, {
+    global: {
+        defaultFontFamily: 'Formular',
+        defaultFontSize: 14,
+    },
+});
 
 export const HistoryChart: React.FC<HistoryChartProps> = props => {
     return <Line data={data} options={options} />;
