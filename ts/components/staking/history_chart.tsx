947021926d75543ae2cf5135882bad759ee1be3a
Rens <hi@rensverschuren.com>
2019-09-23 20:25:23 +0200

    Improve tooltip appearance, disable legend click event

--- ts/components/staking/history_chart.tsx
+++ ts/components/staking/history_chart.tsx
@@ -9,7 +9,6 @@ export interface HistoryChartProps {}
 
 const getDefaultDataset = (color: string) => {
     return {
-        label: 'Fees collected',
         fill: false,
         lineTension: 0,
         backgroundColor: color,
@@ -34,39 +33,47 @@ const data = {
         {
             ...getDefaultDataset(colors.brandLight),
             data: [41, 40, 41, 40, 41, 40, 41, 40, 41, 40, 41, 40],
+            label: 'Fees collected',
         },
         {
             ...getDefaultDataset('#A2F5EB'),
             data: [31, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30],
+            label: 'Rewards shared',
         },
     ],
 };
 
 const options = {
-
     maintainAspectRatio: false,
     scales: {
         yAxes: [
             {
                 scaleLabel: {
                     labelString: 'Fees/rewards (ETH)',
-
+                    display: true,
+                    fontColor: colors.textDarkSecondary,
+                    fontSize: 16,
                 },
                 gridLines: {
                     color: 'transparent',
                 },
                 ticks: {
+                    padding: 30,
                     suggestedMin: 20,
                     suggestedMax: 50,
                     stepSize: 10,
-                    callBack: (value: string, _index: number, _values: string[]) => {
-                        return 'hoi';
+                    precision: 4,
+                    userCallback: (value: string, _index: number, _values: string[]) => {
+                        return `${value}.00`;
                     },
                 },
             },
         ],
         xAxes: [
             {
+                ticks: {
+                    padding: 20,
+                },
                 gridLines: {
                     color: 'transparent',
                 },
@@ -74,9 +81,43 @@ const options = {
         ],
     },
     legend: {
+        position: 'right',
+        fullWidth: true,
         labels: {
             boxWidth: 16,
             fontSize: 14,
+        },
+        onClick: () => {
+            return false;
+        },
+    },
+    layout: {
+        padding: {
+            left: 0,
+        },
+    },
+    tooltips: {
+        cornerRadius: 0,
+        backgroundColor: colors.white,
+        titleFontColor: colors.textDarkSecondary,
+        bodyFontColor: colors.black,
+        footerFontColor: colors.black,
+        titleMarginBottom: 30,
+        titleFontSize: 14,
+        titleFontStyle: '300',
+        bodyFontStyle: 'normal',
+        mode: 'index',
+        bodyFontSize: 18,
+        xPadding: 20,
+        yPadding: 20,
+        borderColor: '#D5D5D5',
+        borderWidth: 1,
+        bodySpacing: 25,
+        callbacks: {
+            label: (item: any, graphData: any) => {
+                const datasetLabel = graphData.datasets[item.datasetIndex].label;
+                return `${datasetLabel} ${item.yLabel} ETH`;
+            },
         },
     },
 };
@@ -85,6 +126,7 @@ merge(defaults, {
     global: {
         defaultFontFamily: 'Formular',
         defaultFontSize: 14,
+        defaultFontStyle: '300',
     },
 });
 
