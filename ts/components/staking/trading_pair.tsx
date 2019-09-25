fe29e379d347e6d315b32192ace36ec4d2114d62
Rens <hi@rensverschuren.com>
2019-09-25 15:19:54 +0200

    Add trading pair component

--- ts/components/staking/trading_pair.tsx
+++ ts/components/staking/trading_pair.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+import { colors } from 'ts/style/colors';
+
 interface Currency {
     name: string;
     iconUrl: string;
@@ -14,23 +16,58 @@ interface TradingPairProps {
     secondCurrency: Currency;
 }
 
-const Container = styled.div`
-
+const Container = styled.a`
+    display: flex;
+    width: 260px;
+    align-items: center;
+    margin-bottom: 20px;
 `;
 
-const TradingPairIcons = styled.ol``;
+const TradingPairIcons = styled.ol`
+    background-color: #f3f3f3;
+    display: block;
+    width: 50%;
+    padding: 10px;
+`;
 
-const TradingPairIcon = styled.img``;
+const TradingPairIcon = styled.img`
+    width: 50px;
+    height: 50px;
+    background-color: ${colors.brandLight};
+    text-align: center;
+    color: ${colors.white};
+    font-weight: 300;
+    display: inline-block;
+    margin-right: 10px;
+    overflow: hidden;
+    &:last-child {
+        margin-right: 0;
+    }
+`;
 
-const TradingPairInfo = styled.div``;
+const TradingPairInfo = styled.div`
+    width: 50%;
+    padding: 10px 0 10px 20px;
+`;
 
-const TradingPairName = styled.div``;
+const TradingPairName = styled.span`
+    display: block;
+    font-size: 20px;
+    line-height: 1.3;
+`;
 
-const Price = styled.div``;
+const Price = styled.span`
+    color: #898989;
+    display: block;
+    line-height: 1.3;
+    font-size: 17px;
+    font-weight: 300;
+    font-feature-settings: 'tnum' on, 'lnum' on;
+`;
 
-export const TradingPair: React.FC<TradingPairProps> = ({ firstCurrency, secondCurrency, price }) => {
+export const TradingPair: React.FC<TradingPairProps> = ({ firstCurrency, secondCurrency, price, url, currency }) => {
     return (
-        <Container>
+        <Container href={url}>
             <TradingPairIcons>
                 <TradingPairIcon src={firstCurrency.iconUrl} alt={firstCurrency.name} />
                 <TradingPairIcon src={secondCurrency.iconUrl} alt={firstCurrency.name} />
@@ -39,7 +76,7 @@ export const TradingPair: React.FC<TradingPairProps> = ({ firstCurrency, secondC
                 <TradingPairName>
                     {firstCurrency.name} - {secondCurrency.name}
                 </TradingPairName>
-                <Price>{price}</Price>
+                <Price>{price} {currency}</Price>
             </TradingPairInfo>
         </Container>
     );
