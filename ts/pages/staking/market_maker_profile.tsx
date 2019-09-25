8cc7f7d9010f3feff724b032df25f780ea8fc62f
Rens <hi@rensverschuren.com>
2019-09-25 15:19:54 +0200

    Add trading pair component

--- ts/pages/staking/market_maker_profile.tsx
+++ ts/pages/staking/market_maker_profile.tsx
@@ -10,43 +10,132 @@ const Container = styled.div`
     max-width: 1152px;
     margin: 0 auto;
     position: relative;
-    height: 290px;
+    padding: 0 20px;
+`;
+
+const Heading = styled.h2`
+    font-size: var(--defaultHeading);
+    line-height: 1.35;
+    margin-bottom: 20px;
+`;
+
+// const TradingPairs = styled.
+
+const TradingPairContainer = styled.div`
+    display: inline-block;
+    width: 330px;
 `;
 
 const tradingPairs = [
     {
         id: '29n5c290cn0cc2943cn239',
-        price: 1200,
+        price: '1,200',
+        url: 'trading-pairs/29n5c290cn0cc2943cn239',
         currency: 'USD',
         firstCurrency: {
-            name: 'BAT',
+            name: 'AIR',
             iconUrl: 'path/to/icon',
         },
         secondCurrency: {
             name: 'BAT',
             iconUrl: 'path/to/icon',
         },
-    }
-
-]
+    },
+    {
+        id: '29n5c290cn0cc2943cn239',
+        price: '1,200',
+        url: 'trading-pairs/29n5c290cn0cc2943cn239',
+        currency: 'USD',
+        firstCurrency: {
+            name: 'AIR',
+            iconUrl: 'path/to/icon',
+        },
+        secondCurrency: {
+            name: 'BAT',
+            iconUrl: 'path/to/icon',
+        },
+    },
+    {
+        id: '29n5c290cn0cc2943cn239',
+        url: 'trading-pairs/29n5c290cn0cc2943cn239',
+        price: '1,200',
+        currency: 'USD',
+        firstCurrency: {
+            name: 'AIR',
+            iconUrl: 'path/to/icon',
+        },
+        secondCurrency: {
+            name: 'BAT',
+            iconUrl: 'path/to/icon',
+        },
+    },
+    {
+        id: '29n5c290cn0cc2943cn239',
+        url: 'trading-pairs/29n5c290cn0cc2943cn239',
+        price: '1,200',
+        currency: 'USD',
+        firstCurrency: {
+            name: 'AIR',
+            iconUrl: 'path/to/icon',
+        },
+        secondCurrency: {
+            name: 'BAT',
+            iconUrl: 'path/to/icon',
+        },
+    },
+    {
+        id: '29n5c290cn0cc2943cn239',
+        url: 'trading-pairs/29n5c290cn0cc2943cn239',
+        price: '1,200',
+        currency: 'USD',
+        firstCurrency: {
+            name: 'AIR',
+            iconUrl: 'path/to/icon',
+        },
+        secondCurrency: {
+            name: 'BAT',
+            iconUrl: 'path/to/icon',
+        },
+    },
+    {
+        id: '29n5c290cn0cc2943cn239',
+        url: 'trading-pairs/29n5c290cn0cc2943cn239',
+        price: '1,200',
+        currency: 'USD',
+        firstCurrency: {
+            name: 'AIR',
+            iconUrl: 'path/to/icon',
+        },
+        secondCurrency: {
+            name: 'BAT',
+            iconUrl: 'path/to/icon',
+        },
+    },
+];
 
 export const MarketMakerProfile: React.FC<MarketMakerProfileProps> = props => {
     return (
         <StakingPageLayout isHome={true} title="Market Maker Profile">
             <Container>
-                {tradingPairs.map(({ price, currency, firstCurrency, secondCurrency, id }) => {
-                    return (
-                        <TradingPair
-                            key={id}
-                            id={id}
-                            price={price}
-                            currency={currency}
-                            firstCurrency={firstCurrency}
-                            secondCurrency={secondCurrency}
-                        />
-                    );
-                })}
-            <Container />
+                <Heading>Trading Pairs</Heading>
+                <div>
+                    {tradingPairs.map(({ price, currency, firstCurrency, secondCurrency, id, url }) => {
+                        return (
+                            <TradingPairContainer key={id}>
+                                <TradingPair
+                                    key={id}
+                                    id={id}
+                                    url={url}
+                                    price={price}
+                                    currency={currency}
+                                    firstCurrency={firstCurrency}
+                                    secondCurrency={secondCurrency}
+                                />
+                            </TradingPairContainer>
+                        );
+                    })}
+                </div>
+            </Container>
         </StakingPageLayout>
     );
 };
