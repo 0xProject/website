81bf44e79290ad9744a38c4f1f4b1cb1c8a0deb1
Rens <hi@rensverschuren.com>
2019-09-30 13:45:50 +0200

    Improve tooltip styling

--- ts/pages/account/account_activity_summary.tsx
+++ ts/pages/account/account_activity_summary.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 import { PanelHeader } from 'ts/components/ui/panel_header';
+
 import { colors } from 'ts/style/colors';
 
 interface AccountActivitySummaryProps {
