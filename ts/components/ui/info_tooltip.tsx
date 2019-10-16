3c7e1979f35e463ac3206e312e02a9453e22f256
Ezekiel Aquino <ezekielaquino@gmail.com>
2019-10-16 10:38:43 +0200

    Refactors info_tooltip, adds info tooltips to staking marketmaker

--- ts/components/ui/info_tooltip.tsx
+++ ts/components/ui/info_tooltip.tsx
@@ -2,16 +2,19 @@ import * as React from 'react';
 import ReactTooltip from 'react-tooltip';
 import styled from 'styled-components';
 
+import { Icon } from 'ts/components/icon';
+
 import { colors } from 'ts/style/colors';
 
 interface InfoTooltipProps {
     children: React.ReactNode;
+    className?: string;
 }
 
 export const InfoTooltip = (props: InfoTooltipProps) => {
     return (
-        <div data-tip={true} data-for="walletBalance" data-border="true">
-            {renderInfoIcon()}
+        <div className={props.className} data-tip={true} data-for="walletBalance" data-border="true">
+            <StyledIcon name="info" size={13} />
 
             <StyledTooltip id="walletBalance" className="tooltip-light">
                 {props.children}
@@ -20,17 +23,11 @@ export const InfoTooltip = (props: InfoTooltipProps) => {
     );
 };
 
-const renderInfoIcon = () => {
-    return (
-        <svg width="7" height="13" viewBox="0 0 7 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g opacity="0.7">
-                <path d="M3.61176 0.888889C3.61176 1.10367 3.43765 1.27778 3.22287 1.27778C3.0081 1.27778 2.83398 1.10367 2.83398 0.888889C2.83398 0.674111 3.0081 0.5 3.22287 0.5C3.43765 0.5 3.61176 0.674111 3.61176 0.888889Z" fill="white" stroke="#5C5C5C"/>
-                <path d="M1 4.88867H3.66667V11.9998" stroke="#5C5C5C" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M1 12H6.33333" stroke="#5C5C5C" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-            </g>
-        </svg>
-    );
-};
+const StyledIcon = styled(Icon)`
+    path {
+        fill: ${colors.textDarkSecondary};
+    }
+`;
 
 const StyledTooltip = styled(ReactTooltip)`
     &.tooltip-light {
