fe8a3bcc944504c63692196a4f0aea9b45527720
fabioberger <me@fabioberger.com>
2019-11-12 13:59:27 +0000

    Remove testnet faucet logic from Portal

--- ts/utils/constants.ts
+++ ts/utils/constants.ts
@@ -82,7 +82,6 @@ export const constants = {
     URL_ECOSYSTEM_BLOG_POST: 'https://blog.0xproject.com/announcing-the-0x-ecosystem-acceleration-program-89d1cb89d565',
     URL_VOTE_BLOG_POST: 'https://blog.0xproject.com/zeip-23-trade-bundles-of-assets-fe69eb3ed960',
     URL_FIREFOX_U2F_ADDON: 'https://addons.mozilla.org/en-US/firefox/addon/u2f-support-add-on/',
-    URL_TESTNET_FAUCET: 'https://faucet.0x.org',
     URL_GITHUB_ORG: 'https://github.com/0xProject',
     URL_GITHUB_WIKI: 'https://github.com/0xProject/wiki',
     URL_FORUM,
