d3e6282ac8beee1d3654a1b763cf44bb86260507
Kim Persson <kimpersson88@gmail.com>
2019-12-09 13:07:29 -0800

    FIXES explicitly trigger staking hook so it can be re-run with same data

--- ts/hooks/use_stake.ts
+++ ts/hooks/use_stake.ts
@@ -16,76 +16,15 @@ export const useStake = () => {
     const networkId = useSelector((state: State) => state.networkId);
     const providerState = useSelector((state: State) => state.providerState);
 
-    const [stakePoolData, setStakePoolData] = React.useState<StakingPoolRecomendation[]>([]);
     const [loadingState, setLoadingState] = React.useState<undefined | TransactionLoadingState>(undefined);
     const [error, setError] = React.useState<Error | undefined>(undefined);
     const [result, setResult] = React.useState<TransactionReceiptWithDecodedLogs | undefined>(undefined);
     const [estimatedTimeMs, setEstimatedTimeMs] = React.useState<number | undefined>(undefined);
 
-    React.useEffect(() => {
+    const depositAndStake = async (stakePoolData: StakingPoolRecomendation[]) => {
         if (!stakePoolData || stakePoolData.length === 0) {
             return;
         }
-
-        const depositAndStake = async () => {
-            setLoadingState(TransactionLoadingState.WaitingForSignature);
-
-            const normalizedPoolData = stakePoolData.map(stakingPoolReccomendation => ({
-                poolId: utils.toPaddedHex(stakingPoolReccomendation.pool.poolId),
-                amountBaseUnits: Web3Wrapper.toBaseUnitAmount(
-                    new BigNumber(stakingPoolReccomendation.zrxAmount, 10),
-                    constants.DECIMAL_PLACES_ZRX,
-                ),
-            }));
-
-            const totalStakeBaseUnits = normalizedPoolData.reduce(
-                (memo: BigNumber, { amountBaseUnits }: { amountBaseUnits: BigNumber }) => {
-                    return memo.plus(amountBaseUnits);
-                },
-                new BigNumber(0, 10),
-            );
-
-            const ownerAddress = (providerState.account as AccountReady).address;
-            const contractAddresses = getContractAddressesForChainOrThrow(networkId);
-
-            const stakingContract = new StakingContract(contractAddresses.stakingProxy, providerState.provider, {
-                from: ownerAddress,
-            });
-            const stakingProxyContract = new StakingProxyContract(
-                contractAddresses.stakingProxy,
-                providerState.provider,
-                {
-                    from: ownerAddress,
-                },
-            );
-
-            const gasInfo = await backendClient.getGasInfoAsync();
-
-            const data = [
-                stakingContract.stake(totalStakeBaseUnits).getABIEncodedTransactionData(),
-                ...normalizedPoolData.map(({ poolId, amountBaseUnits }) =>
-                    stakingContract
-                        .moveStake(
-                            { status: StakeStatus.Undelegated, poolId: constants.STAKING.NIL_POOL_ID }, // From undelegated
-                            { status: StakeStatus.Delegated, poolId }, // To the pool
-                            amountBaseUnits,
-                        )
-                        .getABIEncodedTransactionData(),
-                ),
-            ];
-
-            const txPromise = stakingProxyContract
-                .batchExecute(data)
-                .awaitTransactionSuccessAsync({ from: ownerAddress, gasPrice: gasInfo.gasPriceInWei });
-
-            await txPromise.txHashPromise;
-            setEstimatedTimeMs(gasInfo.estimatedTimeMs);
-            setLoadingState(TransactionLoadingState.WaitingForTransaction);
-            // tslint:disable:await-promise
-            const txResult = await txPromise;
-            setResult(txResult);
-            setLoadingState(TransactionLoadingState.Success);
-        };
 
         if (
             [TransactionLoadingState.WaitingForTransaction, TransactionLoadingState.WaitingForSignature].includes(
@@ -95,18 +34,72 @@ export const useStake = () => {
             return;
         }
 
-        depositAndStake().catch(err => {
-            setLoadingState(TransactionLoadingState.Failed);
-            setError(err);
-            logUtils.log(err);
+        setLoadingState(TransactionLoadingState.WaitingForSignature);
+
+        const normalizedPoolData = stakePoolData.map(stakingPoolReccomendation => ({
+            poolId: utils.toPaddedHex(stakingPoolReccomendation.pool.poolId),
+            amountBaseUnits: Web3Wrapper.toBaseUnitAmount(
+                new BigNumber(stakingPoolReccomendation.zrxAmount, 10),
+                constants.DECIMAL_PLACES_ZRX,
+            ),
+        }));
+
+        const totalStakeBaseUnits = normalizedPoolData.reduce(
+            (memo: BigNumber, { amountBaseUnits }: { amountBaseUnits: BigNumber }) => {
+                return memo.plus(amountBaseUnits);
+            },
+            new BigNumber(0, 10),
+        );
+
+        const ownerAddress = (providerState.account as AccountReady).address;
+        const contractAddresses = getContractAddressesForChainOrThrow(networkId);
+
+        const stakingContract = new StakingContract(contractAddresses.stakingProxy, providerState.provider, {
+            from: ownerAddress,
         });
-    }, [stakePoolData]);
+        const stakingProxyContract = new StakingProxyContract(contractAddresses.stakingProxy, providerState.provider, {
+            from: ownerAddress,
+        });
+
+        const gasInfo = await backendClient.getGasInfoAsync();
+
+        const data = [
+            stakingContract.stake(totalStakeBaseUnits).getABIEncodedTransactionData(),
+            ...normalizedPoolData.map(({ poolId, amountBaseUnits }) =>
+                stakingContract
+                    .moveStake(
+                        { status: StakeStatus.Undelegated, poolId: constants.STAKING.NIL_POOL_ID }, // From undelegated
+                        { status: StakeStatus.Delegated, poolId }, // To the pool
+                        amountBaseUnits,
+                    )
+                    .getABIEncodedTransactionData(),
+            ),
+        ];
+
+        const txPromise = stakingProxyContract
+            .batchExecute(data)
+            .awaitTransactionSuccessAsync({ from: ownerAddress, gasPrice: gasInfo.gasPriceInWei });
+
+        await txPromise.txHashPromise;
+        setEstimatedTimeMs(gasInfo.estimatedTimeMs);
+        setLoadingState(TransactionLoadingState.WaitingForTransaction);
+        // tslint:disable:await-promise
+        const txResult = await txPromise;
+        setResult(txResult);
+        setLoadingState(TransactionLoadingState.Success);
+    };
 
     return {
         loadingState,
         result,
         error,
-        depositAndStake: setStakePoolData,
+        depositAndStake: (stakePoolData: StakingPoolRecomendation[]) => {
+            depositAndStake(stakePoolData).catch((err: Error) => {
+                setLoadingState(TransactionLoadingState.Failed);
+                setError(err);
+                logUtils.log(err);
+            });
+        },
         estimatedTimeMs,
     };
 };
