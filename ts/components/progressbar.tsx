f4c19fbb325e7bbeed8c393fceada8e4a5648710
Kim Persson <kimpersson88@gmail.com>
2020-01-07 15:36:39 +0100

    FIXES handle pools with over 100% estimated stake in progress bar

--- ts/components/progressbar.tsx
+++ ts/components/progressbar.tsx
@@ -8,19 +8,23 @@ interface ProgressbarProps {
 }
 
 interface BarProps {
-    width: number;
+    progress: number;
 }
 
-const Container = styled.div`
+interface ContainerProps {
+    isOverLimit?: boolean;
+}
+
+const Container = styled.div<ContainerProps>`
     height: 5px;
     width: 100%;
-    background-color: ${colors.brandLightest};
+    background-color: ${props => (props.isOverLimit ? colors.error : colors.brandLightest)};
     position: relative;
 `;
 
 const Bar = styled.div<BarProps>`
     transition: width 0.3s ease;
-    width: ${props => props.width}%;
+    width: ${props => props.progress}%;
     background-color: ${colors.brandLight};
     position: absolute;
     top: 0;
@@ -29,9 +33,18 @@ const Bar = styled.div<BarProps>`
 `;
 
 export const Progressbar: React.FC<ProgressbarProps> = (props: ProgressbarProps) => {
+    let adjustedProgress;
+    let isOverLimit;
+    if (props.progress > 100) {
+        adjustedProgress = Math.round((100 / props.progress) * 100);
+        isOverLimit = true;
+    } else {
+        adjustedProgress = props.progress;
+    }
+
     return (
-        <Container>
-            <Bar width={props.progress} />
+        <Container isOverLimit={isOverLimit}>
+            <Bar progress={adjustedProgress} />
         </Container>
     );
 };
