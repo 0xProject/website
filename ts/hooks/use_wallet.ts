602bce5b1f9342954dbcc0dc346ec942ddb32cee
Kim Persson <kimpersson88@gmail.com>
2020-01-08 10:39:46 +0100

    FIXES useWallet hook: split out initial setup, account & network change

--- ts/hooks/use_wallet.ts
+++ ts/hooks/use_wallet.ts
@@ -14,65 +14,80 @@ const PROVIDER_CHAIN_CHANGE_EVENT = 'chainChanged';
 const PROVIDER_ACCOUNTS_CHANGED_EVENT = 'accountsChanged';
 const PROVIDER_NETWORK_CHANGED_EVENT = 'networkChanged';
 
-const PROVIDER_EVENTS = [PROVIDER_CHAIN_CHANGE_EVENT, PROVIDER_ACCOUNTS_CHANGED_EVENT, PROVIDER_NETWORK_CHANGED_EVENT];
-
 export const useWallet = (currentNetworkId: Network, currentProviderState: ProviderState) => {
     const dispatch = useDispatch();
     const [dispatcher, setDispatcher] = useState<Dispatcher | undefined>(undefined);
 
-    const eventListnerFnRef = useRef<() => void | undefined>(undefined);
+    const accountsChangedRef = useRef<() => void | undefined>(undefined);
+    const networkChangedRef = useRef<(networkId: string) => void | undefined>(undefined);
 
     useEffect(() => {
         setDispatcher(new Dispatcher(dispatch));
     }, [dispatch]);
 
-    const handleProviderChanges = useCallback(
-        async (providerState: ProviderState) => {
-            console.log('provider change');
-            const networkId = await providerState.web3Wrapper.getNetworkIdAsync();
-            if (networkId !== currentNetworkId) {
-                console.log('network changed');
+    const handleAccountsChange = useCallback(async () => {
+        console.log('account change');
+        await asyncDispatcher.fetchAccountInfoAndDispatchToStoreAsync(
+            currentProviderState,
+            dispatcher,
+            currentNetworkId,
+            true,
+        );
+    }, [currentNetworkId, currentProviderState, dispatcher]);
+
+    const handleNetworksChange = useCallback(
+        (newNetworkId: string) => {
+            const networkId = parseInt(newNetworkId, 10);
+
+            if (networkId) {
                 dispatcher.updateNetworkId(networkId);
             }
-
-            await asyncDispatcher.fetchAccountInfoAndDispatchToStoreAsync(providerState, dispatcher, networkId, true);
         },
-        [currentNetworkId, dispatcher],
+        [dispatcher],
     );
 
     const cleanupCurrentProvider = useCallback(() => {
         const provider = currentProviderState.provider as any;
 
-        if (provider.off && eventListnerFnRef.current) {
-            for (const eventName of PROVIDER_EVENTS) {
-                provider.off(eventName, eventListnerFnRef.current);
+        if (provider.off) {
+            if (accountsChangedRef.current) {
+                provider.off(PROVIDER_ACCOUNTS_CHANGED_EVENT, accountsChangedRef.current);
+            }
+
+            if (networkChangedRef.current) {
+                provider.off(PROVIDER_NETWORK_CHANGED_EVENT, networkChangedRef.current);
+                provider.off(PROVIDER_CHAIN_CHANGE_EVENT, networkChangedRef.current);
             }
             console.log('cleanup');
         }
     }, [currentProviderState.provider]);
 
-    const connectToWallet = async () => {
+    const connectToWallet = useCallback(async () => {
         cleanupCurrentProvider();
         const providerState = providerStateFactory.getInitialProviderState(currentNetworkId);
 
-        await handleProviderChanges(providerState);
+        const networkId = await providerState.web3Wrapper.getNetworkIdAsync();
+        if (networkId !== currentNetworkId) {
+            dispatcher.updateNetworkId(networkId);
+        }
+
+        await asyncDispatcher.fetchAccountInfoAndDispatchToStoreAsync(providerState, dispatcher, networkId, true);
 
         const newProvider = providerState.provider as any;
         if (newProvider.on) {
-            eventListnerFnRef.current = () => {
-                handleProviderChanges(providerState).catch((err: Error) => {
-                    logUtils.warn(`Failed to update account details: ${err}`);
-                });
-            };
-            for (const eventName of PROVIDER_EVENTS) {
-                newProvider.on(eventName, eventListnerFnRef.current);
+            accountsChangedRef.current = handleAccountsChange;
+            newProvider.on(PROVIDER_ACCOUNTS_CHANGED_EVENT, accountsChangedRef.current);
+
+            networkChangedRef.current = handleNetworksChange;
+            for (const networkEvent of [PROVIDER_NETWORK_CHANGED_EVENT, PROVIDER_CHAIN_CHANGE_EVENT]) {
+                newProvider.on(networkEvent, networkChangedRef.current);
             }
         }
 
         analytics.track(constants.STAKING.TRACKING.CONNECT_WALLET, {
             provider: providerState.displayName || 'UnknownWallet',
         });
-    };
+    }, [cleanupCurrentProvider, currentNetworkId, dispatcher, handleAccountsChange, handleNetworksChange]);
 
     return {
         connectToWallet: () => {
