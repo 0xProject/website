cba300ac2a74b8babea1b1b25d66566bfc37684e
John Johnson <johnrjj@gmail.com>
2020-01-08 13:26:26 -0800

    FIXES active pool filter logic to use OR instead of AND

--- ts/utils/staking_utils.ts
+++ ts/utils/staking_utils.ts
@@ -96,6 +96,6 @@ export const stakingUtils = {
         return `${name.slice(0, 37).trim()}...`;
     },
     isPoolActive: (pool: PoolWithStats) => {
-        return pool.currentEpochStats.totalProtocolFeesGeneratedInEth > 0 && pool.nextEpochStats.zrxStaked > 0;
+        return pool.currentEpochStats.totalProtocolFeesGeneratedInEth > 0 || pool.nextEpochStats.zrxStaked > 0;
     },
 };
