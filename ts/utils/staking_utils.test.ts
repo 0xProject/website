459f207c53a7e404886b44d9f2b0297a0d74c86b
John Johnson <johnrjj@gmail.com>
2020-01-13 13:37:47 -0800

    ADDS sample test for kroeger

--- ts/utils/staking_utils.test.ts
+++ ts/utils/staking_utils.test.ts
@@ -1,13 +1,64 @@
+// tslint:disable: number-literal-format
+
 import { BigNumber } from '@0x/utils';
 import { sum } from 'lodash';
 
 import { stakingUtils } from 'ts/utils/staking_utils';
 
-import { SAMPLE_POOLS } from 'ts/test/fixtures/staking_pools';
+import { createTestPool, SAMPLE_POOLS } from 'ts/test/fixtures/staking_pools';
 
 const { getRecommendedStakingPools } = stakingUtils;
 
-describe('getRecommendedStakingPools', () => {
+describe('getRecommendedStakingPools implementation', () => {
+    test('should correctly calculate', () => {
+        const pool1 = createTestPool({
+            poolId: '1',
+            operatorAddress: '1',
+            currentEpochMockStats: {
+                operatorShare: 0.005,
+                zrxStaked: 1000,
+                approximateStakeRatio: 0.02,
+                totalProtocolFeesGeneratedInEth: 0.0,
+            },
+            nextEpochMockStats: {
+                operatorShare: 0.005,
+                zrxStaked: 1000,
+                approximateStakeRatio: 0.02,
+                totalProtocolFeesGeneratedInEth: 0.0,
+            },
+        });
+        const pool2 = createTestPool({
+            poolId: '2',
+            operatorAddress: '2',
+            currentEpochMockStats: {
+                operatorShare: 0.007,
+                zrxStaked: 2000,
+                approximateStakeRatio: 0.07,
+                totalProtocolFeesGeneratedInEth: 0.001,
+            },
+            nextEpochMockStats: {
+                operatorShare: 0.007,
+                zrxStaked: 2000,
+                approximateStakeRatio: 0.07,
+                totalProtocolFeesGeneratedInEth: 0.001,
+            },
+        });
+
+        const AMOUNT_TO_STAKE = 10000;
+        const recommendedPools = getRecommendedStakingPools(AMOUNT_TO_STAKE, [pool1, pool2]);
+
+        // Assert on results..
+        expect(recommendedPools).toHaveLength(2);
+
+        expect(recommendedPools[0].zrxAmount).toEqual(6666.67);
+        expect(recommendedPools[0].pool.poolId).toEqual('1');
+
+        expect(recommendedPools[1].zrxAmount).toEqual(3333.33);
+        expect(recommendedPools[1].pool.poolId).toEqual('2');
+    });
+});
+
+describe('getRecommendedStakingPools rounding', () => {
     test('should not break on zero', () => {
         const totalRequestedToStake = 0;
         const recommendedPools = getRecommendedStakingPools(totalRequestedToStake, SAMPLE_POOLS);
