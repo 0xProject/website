4d2fd876e38690a845053888d57de5255371133b
fragosti <francesco.agosti93@gmail.com>
2020-01-14 17:19:34 -0800

    Change name of API tab in docs

--- ts/components/docs/header/header.tsx
+++ ts/components/docs/header/header.tsx
@@ -43,7 +43,7 @@ const navItems: INavItems[] = [
     },
     {
         id: 'api',
-        text: 'API Explorer',
+        text: 'API Specification',
         url: WebsitePaths.DocsApi,
     },
     {
