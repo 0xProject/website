8a2b9f6f5ba3129b0e4b466fe6cfae02db6635aa
Paulius <paulius@bakkenbaeck.no>
2020-06-30 17:42:04 +0300

    Add description section with cases

--- ts/pages/mesh.tsx
+++ ts/pages/mesh.tsx
@@ -2,14 +2,34 @@ import * as React from 'react';
 
 import { Blockquote } from 'ts/components/blockquote';
 import { Button } from 'ts/components/button';
-import { Section } from 'ts/components/newLayout';
+import { Column, Section } from 'ts/components/newLayout';
+import { Definition } from 'ts/components/definition';
 import { DocumentTitle } from 'ts/components/document_title';
 import { Hero } from 'ts/components/hero';
+import { Heading, Paragraph } from 'ts/components/text';
 import { SiteWrap } from 'ts/components/siteWrap';
 
 import { MeshStats } from 'ts/pages/mesh/mesh_stats';
 
 import { documentConstants } from 'ts/utils/document_meta_constants';
+
+const descriptionCasesData = [
+    {
+        title: 'Decentralized',
+        description: 'No listing process, censorship free, globally accessible, and unstoppable',
+        icon: 'networkedLiquidity',
+    },
+    {
+        title: 'Limit orders, for more control',
+        description: 'Create markets with more granular control over pricing than AMMs like uniswap.',
+        icon: 'orderBooks',
+    },
+    {
+        title: 'Supports any asset, including NFTs',
+        description: 'Use 0x mesh to host a wide variety of asset types, including non-fungible tokens',
+        icon: 'tokens',
+    },
+];
 
 export class Mesh extends React.Component {
     public render(): React.ReactNode {
@@ -43,7 +63,40 @@ export class Mesh extends React.Component {
                         global consensus is not required
                     </Blockquote>
                 </Section>
+
+                <Section bgColor="dark" isFlex={true} maxWidth="1170px">
+                    <Column width="50%" maxWidth="550px">
+                        <Heading asElement="h2" size="medium">
+                            Fully Decentralized, Highly Flexible
+                        </Heading>
+                        <Paragraph size="medium" isMuted={true} padding={0}>
+                            The most flexible solution out there, you can build anything you want, however you want.
+                        </Paragraph>
+                    </Column>
+
+                    <Column width="46%" maxWidth="490px">
+                        {descriptionCasesData.map(item => (
+                            <Definition
+                                key={item.title}
+                                title={item.title}
+                                titleSize="small"
+                                description={<Description content={item.description} />}
+                                icon={item.icon}
+                                iconSize={80}
+                                isInlineIcon={true}
+                                inlineIconSpacing="30px"
+                                isWithMargin={true}
+                            />
+                        ))}
+                    </Column>
+                </Section>
             </SiteWrap>
         );
     }
 }
+
+const Description: React.FC<{ content: string }> = ({ content }) => (
+    <Paragraph isMuted={true} padding={0} isNoMargin={true}>
+        {content}
+    </Paragraph>
+);
