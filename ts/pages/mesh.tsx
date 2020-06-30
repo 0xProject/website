4ebe52a6189aa49e050ef09ca8982ff2c3c840f3
Paulius <paulius@bakkenbaeck.no>
2020-06-30 17:40:40 +0300

    Add blockquote section

--- ts/pages/mesh.tsx
+++ ts/pages/mesh.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
+import { Blockquote } from 'ts/components/blockquote';
 import { Button } from 'ts/components/button';
+import { Section } from 'ts/components/newLayout';
 import { DocumentTitle } from 'ts/components/document_title';
 import { Hero } from 'ts/components/hero';
 import { SiteWrap } from 'ts/components/siteWrap';
@@ -29,6 +31,18 @@ export class Mesh extends React.Component {
                         </Button>
                     }
                 />
+
+                {/* Prevent double spacing between hero and this section */}
+                <Section isWrapped={false} padding="28px 0 120px" paddingMobile="8px 0 40px" overflow="visible">
+                    <Blockquote
+                        citeUrl="https://twitter.com/VitalikButerin/status/1249421031682510849"
+                        citeLabel="Vitalik Buterin, Twitter"
+                    >
+                        Non-blockchain decentralized message-passing networks (eg. as used by @0xProject @ethstatus) can
+                        be a very valuable complement to blockchains, and offer important efficiencies in areas where
+                        global consensus is not required
+                    </Blockquote>
+                </Section>
             </SiteWrap>
         );
     }
