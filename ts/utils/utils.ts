c9a4e08c0be100bdee09b005371533ed2884407e
Ben Lyaunzon <lyaunzon.b@gmail.com>
2021-04-01 01:41:36 -0700

    hardcode staging api for testing purposes

--- ts/utils/utils.ts
+++ ts/utils/utils.ts
@@ -405,14 +405,14 @@ export const utils = {
         return configs.BACKEND_BASE_PROD_URL;
     },
     getAPIBaseUrl(networkId: Network): string {
-        if (environments.isDevelopment()) {
-            return configs.API_BASE_DEV_URL;
-        } else if (networkId === Network.Kovan) {
-            return configs.API_BASE_KOVAN_URL;
-        } else if (environments.isDogfood()) {
-            return configs.API_BASE_STAGING_URL;
-        }
-        return configs.API_BASE_PROD_URL;
+        // if (environments.isDevelopment()) {
+        //     return configs.API_BASE_DEV_URL;
+        // } else if (networkId === Network.Kovan) {
+        //     return configs.API_BASE_KOVAN_URL;
+        // } else if (environments.isDogfood()) {
+        //     return configs.API_BASE_STAGING_URL;
+        // }
+        return configs.API_BASE_STAGING_URL;
     },
     isExternallyInjected(providerType: ProviderType, injectedProviderName: string): boolean {
         return providerType === ProviderType.Injected && injectedProviderName !== constants.PROVIDER_NAME_PUBLIC;
