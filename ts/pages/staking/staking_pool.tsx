411e434afe3de33b496b27b61b70325a89121fab
Ben Lyaunzon <lyaunzon.b@gmail.com>
2021-04-08 19:33:29 -0700

    further formatting and 12 day epoch avg apy

--- ts/pages/staking/staking_pool.tsx
+++ ts/pages/staking/staking_pool.tsx
@@ -14,7 +14,7 @@ import { InfoTooltip } from 'ts/components/ui/info_tooltip';
 import { useAPIClient } from 'ts/hooks/use_api_client';
 
 import { State } from 'ts/redux/reducer';
-import { PoolEpochRewards, PoolWithHistoricalStats, WebsitePaths } from 'ts/types';
+import { EpochPoolStats, PoolEpochRewards, PoolWithHistoricalStats, WebsitePaths } from 'ts/types';
 import { errorReporter } from 'ts/utils/error_reporter';
 import { formatEther, formatPercent, formatZrx } from 'ts/utils/format_number';
 import { stakingUtils } from 'ts/utils/staking_utils';
@@ -61,34 +61,39 @@ export const StakingPool: React.FC<StakingPoolProps & RouteChildrenProps> = (pro
     const apiClient = useAPIClient(networkId);
     const [stakingPool, setStakingPool] = useState<PoolWithHistoricalStats | undefined>(undefined);
     const [epochRewards, setEpochRewards] = useState<PoolEpochRewards[] | undefined>(undefined);
-    const [stakingPoolAPY, setStakingPoolAPY] = useState<number | undefined>(undefined);
+    const [stakingPoolAPY3Epochs, setStakingPoolAPY3Epochs] = useState<number | undefined>(undefined);
+    const [stakingPoolAPY12Epochs, setStakingPoolAPY12Epochs] = useState<number | undefined>(undefined);
 
     useEffect(() => {
-        const calcAverageEpochApy = (numEpochs: number, rewards: PoolEpochRewards[]) => {
-            const rewardsToAverage =
-                rewards.length > numEpochs ? rewards.slice(Math.max(rewards.length - numEpochs, 0)) : rewards;
+        const calcShortTermAndLongTermAPY = (rewards: PoolEpochRewards[]) => {
+            const average = (arr: number[]) => arr.reduce((sum, el) => sum + el, 0) / arr.length;
 
-            const historicalAPYs = rewardsToAverage.map((reward) => {
+            const epochRewardAPYs = rewards.map((reward) => {
                 return reward.apy;
             });
-            const average = (arr: number[]) => arr.reduce((sum, el) => sum + el, 0) / arr.length;
-            setStakingPoolAPY(average(historicalAPYs));
+            const rewardsToAverageShortTerm =
+                epochRewardAPYs.length > 3 ? epochRewardAPYs.slice(Math.max(rewards.length - 3, 0)) : epochRewardAPYs;
+
+            const rewardsToAverageLongTerm =
+                epochRewardAPYs.length > 12 ? epochRewardAPYs.slice(Math.max(rewards.length - 12, 0)) : epochRewardAPYs;
+
+            setStakingPoolAPY3Epochs(average(rewardsToAverageShortTerm));
+            setStakingPoolAPY12Epochs(average(rewardsToAverageLongTerm));
         };
         apiClient
             .getStakingPoolByIdAsync(poolId)
             .then((res) => {
                 setStakingPool(res.stakingPool);
-                apiClient
-                    .getStakingPoolRewardsAsync(poolId)
-                    .then((resRewards) => {
-                        // fix this, shouldnt be unnecessarily nested
-                        setEpochRewards(resRewards.stakingPoolRewards.epochRewards);
-                        calcAverageEpochApy(7, resRewards.stakingPoolRewards.epochRewards);
-                    })
-                    .catch((err: Error) => {
-                        logUtils.warn(err);
-                        errorReporter.report(err);
-                    });
+            })
+            .catch((err: Error) => {
+                logUtils.warn(err);
+                errorReporter.report(err);
+            });
+        apiClient
+            .getStakingPoolRewardsAsync(poolId)
+            .then((resRewards) => {
+                setEpochRewards(resRewards.stakingPoolRewards.epochRewards);
+                calcShortTermAndLongTermAPY(resRewards.stakingPoolRewards.epochRewards);
             })
             .catch((err: Error) => {
                 logUtils.warn(err);
@@ -101,143 +106,148 @@ export const StakingPool: React.FC<StakingPoolProps & RouteChildrenProps> = (pro
         return <Redirect to={WebsitePaths.Staking} />;
     }
 
-    if (!stakingPool) {
-        return null;
+    let currentEpoch: EpochPoolStats,
+        nextEpoch,
+        historicalEpochs,
+        zrxStakeChangeBetweenEpochs: number,
+        zrxToStaked = null;
+    if (stakingPool) {
+        currentEpoch = stakingPool.currentEpochStats;
+        nextEpoch = stakingPool.nextEpochStats;
+
+        // Reminder: stake change can be negative
+        zrxStakeChangeBetweenEpochs = new BigNumber(nextEpoch.zrxStaked || 0)
+            .minus(new BigNumber(currentEpoch.zrxStaked || 0))
+            .toNumber();
+
+        // Only allow epochs that have finished into historical data
+        historicalEpochs = epochRewards
+            ? epochRewards
+                  .filter((x) => !!x.epochEndTimestamp)
+                  .sort((a, b) => {
+                      return a.epochId - b.epochId;
+                  })
+            : null;
+
+        const fullyStakedZrx = nextEpoch.zrxStaked / (nextEpoch.approximateStakeRatio || 1);
+        zrxToStaked = Math.max(fullyStakedZrx - nextEpoch.zrxStaked, 0);
     }
-
-    const currentEpoch = stakingPool.currentEpochStats;
-    const nextEpoch = stakingPool.nextEpochStats;
-
-    // Reminder: stake change can be negative
-    const zrxStakeChangeBetweenEpochs = new BigNumber(nextEpoch.zrxStaked || 0)
-        .minus(new BigNumber(currentEpoch.zrxStaked || 0))
-        .toNumber();
-
-    // Only allow epochs that have finished into historical data
-    const historicalEpochs = epochRewards
-        ? epochRewards
-              .filter((x) => !!x.epochEndTimestamp)
-              .sort((a, b) => {
-                  return a.epochId - b.epochId;
-              })
-        : null;
-
-    const fullyStakedZrx = nextEpoch.zrxStaked / (nextEpoch.approximateStakeRatio || 1);
-    const zrxToStaked = Math.max(fullyStakedZrx - nextEpoch.zrxStaked, 0);
 
     return (
         <StakingPageLayout isHome={true} title="Staking pool">
-            <DashboardHero
-                title={stakingUtils.getPoolDisplayName(stakingPool)}
-                websiteUrl={stakingPool.metaData.websiteUrl}
-                poolId={stakingPool.poolId}
-                operatorAddress={stakingPool.operatorAddress}
-                isVerified={stakingPool.metaData.isVerified}
-                estimatedStake={nextEpoch.approximateStakeRatio * 100}
-                zrxToStaked={zrxToStaked}
-                rewardsShared={(1 - nextEpoch.operatorShare) * 100}
-                iconUrl={stakingPool.metaData.logoUrl}
-                networkId={networkId}
-                tabs={[
-                    {
-                        title: 'Current Epoch',
-                        metrics: [
-                            // todo(johnrjj) Cutting volume for MVP
-                            // {
-                            //     title: 'Total Volume',
-                            //     number: '1.23M USD',
-                            // },
-                            {
-                                title: 'APY (last 7 epochs)',
-                                number: `${formatPercent(stakingPoolAPY * 100 || 0).minimized}%`,
-                            },
-                            {
-                                title: 'Fees Generated',
-                                // 4 decimals looks better here to keep it from wrapping
-                                number: `${
-                                    formatEther(currentEpoch.totalProtocolFeesGeneratedInEth, {
-                                        decimals: 4,
-                                        decimalsRounded: 4,
-                                    }).minimized
-                                } ETH`,
-                            },
-                            {
-                                title: 'ZRX Staked',
-                                number: `${formatZrx(nextEpoch.zrxStaked, { bigUnitPostfix: true }).formatted}`,
-                                headerComponent: () => (
-                                    <InfoTooltip id="next-epoch-staked-balance">
-                                        <div>
+            {stakingPool && (
+                <DashboardHero
+                    title={stakingUtils.getPoolDisplayName(stakingPool)}
+                    websiteUrl={stakingPool.metaData.websiteUrl}
+                    poolId={stakingPool.poolId}
+                    operatorAddress={stakingPool.operatorAddress}
+                    isVerified={stakingPool.metaData.isVerified}
+                    estimatedStake={nextEpoch.approximateStakeRatio * 100}
+                    zrxToStaked={zrxToStaked}
+                    rewardsShared={(1 - nextEpoch.operatorShare) * 100}
+                    iconUrl={stakingPool.metaData.logoUrl}
+                    networkId={networkId}
+                    tabs={[
+                        {
+                            title: 'Current Epoch',
+                            metrics: [
+                                // todo(johnrjj) Cutting volume for MVP
+                                // {
+                                //     title: 'Total Volume',
+                                //     number: '1.23M USD',
+                                // },
+                                {
+                                    title: 'APY (last 3 epochs)',
+                                    number: `${formatPercent(stakingPoolAPY3Epochs * 100 || 0).minimized}%`,
+                                },
+                                {
+                                    title: 'Fees Generated',
+                                    // 4 decimals looks better here to keep it from wrapping
+                                    number: `${
+                                        formatEther(currentEpoch.totalProtocolFeesGeneratedInEth, {
+                                            decimals: 2,
+                                            decimalsRounded: 2,
+                                        }).minimized
+                                    } ETH`,
+                                },
+                                {
+                                    title: 'ZRX Staked',
+                                    number: `${formatZrx(nextEpoch.zrxStaked, { bigUnitPostfix: true }).formatted}`,
+                                    headerComponent: () => (
+                                        <InfoTooltip id="next-epoch-staked-balance">
                                             <div>
-                                                <TooltipLabel>Current Epoch:</TooltipLabel>{' '}
-                                                {
-                                                    formatZrx(currentEpoch.zrxStaked, {
-                                                        decimals: 2,
-                                                        decimalsRounded: 2,
-                                                        roundDown: true,
-                                                    }).full
-                                                }
+                                                <div>
+                                                    <TooltipLabel>Current Epoch:</TooltipLabel>{' '}
+                                                    {
+                                                        formatZrx(currentEpoch.zrxStaked, {
+                                                            decimals: 2,
+                                                            decimalsRounded: 2,
+                                                            roundDown: true,
+                                                        }).full
+                                                    }
+                                                </div>
+                                                <div>
+                                                    <TooltipLabel>Pending Epoch:</TooltipLabel>{' '}
+                                                    {
+                                                        formatZrx(zrxStakeChangeBetweenEpochs, {
+                                                            decimals: 2,
+                                                            decimalsRounded: 2,
+                                                            roundDown: true,
+                                                            positiveSign: zrxStakeChangeBetweenEpochs >= 0,
+                                                        }).full
+                                                    }
+                                                </div>
                                             </div>
-                                            <div>
-                                                <TooltipLabel>Pending Epoch:</TooltipLabel>{' '}
-                                                {
-                                                    formatZrx(zrxStakeChangeBetweenEpochs, {
-                                                        decimals: 2,
-                                                        decimalsRounded: 2,
-                                                        roundDown: true,
-                                                        positiveSign: zrxStakeChangeBetweenEpochs >= 0,
-                                                    }).full
-                                                }
-                                            </div>
-                                        </div>
-                                    </InfoTooltip>
-                                ),
-                            },
+                                        </InfoTooltip>
+                                    ),
+                                },
 
-                            {
-                                title: 'Rewards Shared',
-                                // No good way to show rewards shared of an epoch in progress (currentEpoch) right now.
-                                // Defaulting to dash ('-') for now
-                                number: `${
-                                    formatEther('0', {
-                                        zeroStyled: true,
-                                    }).formatted
-                                } ETH`,
-                            },
-                        ],
-                    },
-                    {
-                        title: 'All Time',
-                        metrics: [
-                            {
-                                title: 'ZRX Staked',
-                                number: `${formatZrx(currentEpoch.zrxStaked).minimized}`,
-                            },
-                            {
-                                title: 'Fees Generated',
-                                number: `${
-                                    formatEther(stakingPool.allTimeStats.protocolFeesGeneratedInEth, {
-                                        decimalsRounded: 4,
-                                        decimals: 4,
-                                    }).formatted
-                                } ETH`,
-                            },
-                            {
-                                title: 'Rewards Shared',
-                                number: `${
-                                    formatEther(stakingPool.allTimeStats.membersRewardsPaidInEth, {
-                                        decimals: 4,
-                                        decimalsRounded: 4,
-                                    }).formatted
-                                } ETH`,
-                            },
-                            {
-                                title: 'Number of trades',
-                                number: stakingPool.allTimeStats.numberOfFills,
-                            },
-                        ],
-                    },
-                ]}
-            />
+                                {
+                                    title: 'Rewards Shared',
+                                    // No good way to show rewards shared of an epoch in progress (currentEpoch) right now.
+                                    // Defaulting to dash ('-') for now
+                                    number: `${
+                                        formatEther('0', {
+                                            zeroStyled: true,
+                                        }).formatted
+                                    } ETH`,
+                                },
+                            ],
+                        },
+                        {
+                            title: 'All Time',
+                            metrics: [
+                                {
+                                    title: 'APY (last 12 epochs)',
+                                    number: `${formatPercent(stakingPoolAPY12Epochs * 100 || 0).minimized}%`,
+                                },
+                                {
+                                    title: 'Fees Generated',
+                                    number: `${
+                                        formatEther(stakingPool.allTimeStats.protocolFeesGeneratedInEth, {
+                                            decimalsRounded: 2,
+                                            decimals: 2,
+                                        }).formatted
+                                    } ETH`,
+                                },
+                                {
+                                    title: 'ZRX Staked',
+                                    number: `${formatZrx(nextEpoch.zrxStaked, { bigUnitPostfix: true }).formatted}`,
+                                },
+                                {
+                                    title: 'Rewards Shared',
+                                    number: `${
+                                        formatEther(stakingPool.allTimeStats.membersRewardsPaidInEth, {
+                                            decimals: 2,
+                                            decimalsRounded: 2,
+                                        }).formatted
+                                    } ETH`,
+                                },
+                            ],
+                        },
+                    ]}
+                />
+            )}
             {/* TODO(johnrjj) Copy from account page when finished */}
             {/* <ActionsWrapper>
                 <ActionsInner>
