9daff0899262ae4c4b7a3d83ed65d8d36b9c78e5
Ben Lyaunzon <lyaunzon.b@gmail.com>
2021-05-30 23:50:32 -0700

    lintfix

--- ts/components/sections/landing/hero.tsx
+++ ts/components/sections/landing/hero.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import styled from 'styled-components';
 import { animated, useTransition } from 'react-spring-latest';
+import styled from 'styled-components';
 
 import { Button } from 'ts/components/button';
 import { Hero } from 'ts/components/hero';
