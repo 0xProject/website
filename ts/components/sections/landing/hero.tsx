acabdfd2bd81fd79861f5408908b1cbb1c9803ef
Ben Lyaunzon <lyaunzon.b@gmail.com>
2021-05-30 23:47:03 -0700

    update landing page hero w/ multi-chain anim

--- ts/components/sections/landing/hero.tsx
+++ ts/components/sections/landing/hero.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { animated, useTransition } from 'react-spring-latest';
 
 import { Button } from 'ts/components/button';
 import { Hero } from 'ts/components/hero';
@@ -11,11 +12,76 @@ import { WebsitePaths } from 'ts/types';
 
 export interface SectionlandingHeroProps {}
 
+const AnimatedHeroTitle: React.FC<{}> = () => {
+    const [index, set] = React.useState(0);
+
+    const slides = [
+        {
+            label: 'Ethereum',
+            color: '#7694FF',
+        },
+        {
+            label: 'Polygon',
+            color: '#794FDD',
+        },
+        { label: 'BSC', color: '#F5AA15' },
+    ];
+
+    const transitions = useTransition(index, {
+        key: index,
+        from: {
+            transform: 'translate3d(0,50px,0)',
+            opacity: 0,
+        },
+        enter: { transform: 'translate3d(0,0px,0)', opacity: 1 },
+        leave: { transform: 'translate3d(0,-50px,0)', opacity: 0 },
+    });
+
+    React.useEffect(() => {
+        const t = setInterval(() => {
+            set((state) => {
+                return (state + 1) % slides.length;
+            });
+        }, 1500);
+        return () => clearTimeout(t);
+    }, []);
+
+    return (
+        <span>
+            The liquidity endpoint for DeFi on
+            <div
+                style={{
+                    height: '90px',
+                    overflow: 'hidden',
+                    display: 'inline-block',
+                    position: 'absolute',
+                    width: '80%',
+                }}
+            >
+                {transitions((style, i) => {
+                    return (
+                        <animated.span
+                            style={{
+                                ...style,
+                                position: 'absolute',
+                                marginLeft: '1rem',
+                                color: slides[i].color,
+                            }}
+                        >
+                            {slides[i].label}
+                        </animated.span>
+                    );
+                })}
+            </div>
+        </span>
+    );
+};
+
 export const SectionLandingHero: React.FC<SectionlandingHeroProps> = () => {
     return (
         <>
             <Hero
-                title={<span>The liquidity endpoint for DeFi</span>}
+                title={<AnimatedHeroTitle />}
                 isLargeTitle={true}
                 isFullWidth={true}
                 maxWidth={'1280px'}
