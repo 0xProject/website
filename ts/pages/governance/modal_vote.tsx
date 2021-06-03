b908b1631c38a0cec6c538f5d0f2b9527025c942
Ben Lyaunzon <lyaunzon.b@gmail.com>
2021-06-03 10:14:58 -0700

    remove logging

--- ts/pages/governance/modal_vote.tsx
+++ ts/pages/governance/modal_vote.tsx
@@ -226,15 +226,6 @@ export const ModalTreasuryVote: React.FC<ModalVoteProps> = ({
                     userOperatedPools ? userOperatedPools.map((pool) => encodePoolId(parseInt(pool.poolId, 10))) : [],
                 )
                 .callAsync();
-            const uop = userOperatedPools
-                ? userOperatedPools.map((pool) => encodePoolId(parseInt(pool.poolId, 10)))
-                : [];
-            // tslint:disable-next-line
-            console.log(
-                `fetched voting power | account: ${
-                    account.address
-                } | uop: ${uop} | voting power: ${votingPower.toNumber()}`,
-            );
             const votingPowerBigNumber = new BigNumber(votingPower.toNumber());
             setCurrentVotingPower(votingPowerBigNumber);
         };
