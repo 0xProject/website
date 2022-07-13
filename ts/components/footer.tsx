12e527558af8b20751e4b254f08bcaa37a162134
Dennis Simon <dennism.simon94@gmail.com>
2022-07-13 19:40:32 +0200

    Add Stack Exchange link to footer

--- ts/components/footer.tsx
+++ ts/components/footer.tsx
@@ -34,6 +34,11 @@ const linkRows: LinkRows[] = [
             { url: 'https://docs.0x.org/', text: 'Documentation', shouldOpenInNewTab: true },
             { url: constants.URL_GITHUB_ORG, text: 'GitHub', shouldOpenInNewTab: true },
             { url: 'https://docs.0x.org/protocol/docs', text: 'Protocol Spec', shouldOpenInNewTab: true },
+            {
+                url: 'https://ethereum.stackexchange.com/questions/tagged/0x',
+                text: 'Stack Exchange',
+                shouldOpenInNewTab: true,
+            },
         ],
     },
     {
