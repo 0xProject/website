9f213cfd864c51d2944a817f1ace52b9650c6b6a
Dennis Simon <dennism.simon94@gmail.com>
2023-04-17 10:31:40 +0200

    Add new logos

--- ts/components/logo.tsx
+++ ts/components/logo.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-import LogoIcon from 'ts/icons/logo-with-type.svg';
+import LogoIconShort from 'ts/icons/logo-governance-short.svg';
+import LogoIcon from 'ts/icons/logo-governance.svg';
 import { IThemeInterface } from 'ts/style/theme';
 
 import { zIndex } from 'ts/style/z_index';
@@ -17,16 +18,40 @@ const StyledLogo = styled.div`
     text-align: left;
     position: relative;
     z-index: ${zIndex.header};
+    height: 31px;
+    transform: translateY(-15%);
 
     @media (max-width: 800px) {
         svg {
-            width: 60px;
+            height: 31px;
         }
     }
 `;
 
 const Icon = styled(LogoIcon)<LogoInterface>`
     flex-shrink: 0;
+
+    height: 31px;
+    display: block;
+
+    @media (max-width: 800px) {
+        display: none;
+    }
+
+    path {
+        fill: ${(props) => props.theme.textColor};
+    }
+`;
+
+const IconShort = styled(LogoIconShort)<LogoInterface>`
+    flex-shrink: 0;
+
+    height: 31px;
+    display: none;
+
+    @media (max-width: 800px) {
+        display: block;
+    }
 
     path {
         fill: ${(props) => props.theme.textColor};
@@ -36,5 +61,6 @@ const Icon = styled(LogoIcon)<LogoInterface>`
 export const Logo: React.StatelessComponent<LogoInterface> = (props: LogoInterface) => (
     <StyledLogo>
         <Icon {...props} />
+        <IconShort />
     </StyledLogo>
 );
