5f13d0054842ec6ccb3b546c099118ade718e7fa
Dennis Simon <dennis@0xproject.com>
2026-01-22 01:48:54 +0900

    update alchemy endpoint

--- ts/utils/configs.ts
+++ ts/utils/configs.ts
@@ -72,7 +72,7 @@ export const configs = {
         [1]: [
             environments.isDevelopment()
                 ? 'https://mainnet.0x.org'
-                : 'https://eth-mainnet.alchemyapi.io/v2/8JwI7bMSK8ojsPDbyeHt6NK8w23afo1q',
+                : 'https://eth-mainnet.g.alchemy.com/v2/8JwI7bMSK8ojsPDbyeHt6NK8w23afo1q',
         ], // [`https://mainnet.infura.io/v3/${INFURA_API_KEY}`, 'https://mainnet.0x.org'],
         [42]: [`https://kovan.infura.io/v3/${INFURA_API_KEY}`, 'https://kovan.0x.org'],
         [3]: [`https://ropsten.infura.io/v3/${INFURA_API_KEY}`],
